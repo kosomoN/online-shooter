d539baed2ba0428959aa30e357f25fc018fcf385
Hugo Holmqvist <hugo.holm@live.com>
2015-11-21 21:51:04 +0200

    Fix
    fixfixfixfixfix

--- dayz2/GlobalSystem.h
+++ dayz2/GlobalSystem.h
@@ -6,10 +6,12 @@
 #include "SpriteBatch.h"
 #include "StateSystem.h"
 #include "SpriteRenderer.h"
+#include "PlayerController.h"
 #include "Client.h"
 #include <cstdint>
 
 typedef std::string string;
+typedef unsigned int uint;
 
 class CGlobalSystem
 {
@@ -24,6 +26,7 @@ public:
 	CSpriteBatch* pSpriteBatch;
 	CStateSystem* pStateSystem;
 	CSpriteRenderer* pSpriteRenderer;
+	CPlayerController* pPlayerController;
 	CClient* pClient;
 
 	void log(string);
