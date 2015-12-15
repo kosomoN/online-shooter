a6e5d5cf60681443ef3d3b70395499df3d5935c8
Onni Kosomaa <onni.kosomaa@hotmail.com>
2015-12-15 18:38:15 +0200

    Fix blood rendering on top of players

--- dayz2/SpriteRenderer.cpp
+++ dayz2/SpriteRenderer.cpp
@@ -38,8 +38,17 @@ void CSpriteRenderer::removeSprite(CSprite * pSprite)
 
 CSprite* CSpriteRenderer::addSprite(float width, float height, float u1, float v1, float u2, float v2, std::string texture)
 {
+	return addSprite(width, height, u1, v1, u2, v2, texture, true);
+}
+
+//TODO Make better layering
+CSprite* CSpriteRenderer::addSprite(float width, float height, float u1, float v1, float u2, float v2, std::string texture, bool end)
+{
 	auto pSprite = new CSprite(width, height, u1, v1, u2, v2, gSys->pTextureCache->getTexture(texture));
-	m_sprites.push_back(pSprite);
+	if (end)
+		m_sprites.push_back(pSprite);
+	else
+		m_sprites.insert(m_sprites.begin(), pSprite);
 	return pSprite;
 }
 
--- dayz2/SpriteRenderer.h
+++ dayz2/SpriteRenderer.h
@@ -13,6 +13,7 @@ public:
 	void renderSprites();
 	void removeSprite(CSprite* pSprite);
 	CSprite* addSprite(float width, float height, float u1, float v1, float u2, float v2, std::string texture);
+	CSprite* addSprite(float width, float height, float u1, float v1, float u2, float v2, std::string texture, bool end);
 private:
 	std::vector<CSprite*> m_sprites;
 	
